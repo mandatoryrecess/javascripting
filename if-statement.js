99de39a302ed0efe84c2b79ec5aca61fac70e8e7
mandatoryrecess <rossanneclamp@gmail.com>
2021-03-31 14:26:19 +0000

    Initial commit with all my work so far

---
+++ if-statement.js
@@ -0,0 +1,7 @@
+let fruit = 'orange'
+let x = fruit.length 
+if (x > 5) {
+console.log("The fruit name has more than five characters.")
+} else {
+    console.log("the fruit has less than five characters.")
+  }
