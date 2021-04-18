3e20cdf95b5395ce3a4adfeae13265582bac98f9
mandatoryrecess <rossanneclamp@gmail.com>
2021-04-18 17:15:25 +0000

    node school

---
+++ object-keys.js
@@ -0,0 +1,10 @@
+const car = {  
+  make: 'VW',  
+  model: 'Golf - R',  
+  year: 2017,
+  color: 'white', 
+  name: 'panada',  
+}  
+const keys = Object.keys(car)  
+  
+console.log(keys) 
---
+++ object-properties.js
@@ -0,0 +1,14 @@
+const food = {  
+  types: 'only pizza'  
+} 
+console.log(food.types);
+
+const favFood = {
+  types: 'salmon'
+}
+console.log(favFood['types']);
+
+const lamp = {
+  brightness: 8
+}
+console.log("the lamp is " + (lamp['brightness']) + " level of brightness");
